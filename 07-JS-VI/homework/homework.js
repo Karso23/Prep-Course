100a46779e8606600b554d58c3dab17d78630da6
Karso23 <karsoreef@hotmail.com>
2022-09-02 21:33:08 -0500

    Homework 07 realizada!

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,30 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+ 
+  let primerLetra = nombre.charAt().toUpperCase();
+  let strNuevo = primerLetra;
+
+  for(let i=1; i<nombre.length; i++){
+    strNuevo = strNuevo + (nombre[i]);
+  }
+  return strNuevo;
+
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb(cb)
 }
+ 
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2)
+
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +35,26 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  suma = numeros.reduce(function(element,index){
+    return (element+index)
+  })
+
+  cb(suma)
+  
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  
+  //***Aqui lo resolvi con la sintaxix de una funcion normal***//
+  /*array.forEach(function(elemento){
+    cb(elemento)
+  })
+*/
+//***Aqui lo resolvi con la sintaxis de una Arrow Function***//
+  array.forEach((elemento) => cb(elemento));
 }
 
 function map(array, cb) {
@@ -35,12 +62,29 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  
+  const newArray = [];
+
+  //Metodo con la sintaxis de una funcion normal
+  /*array.map(function(elemento){
+    newArray.push(cb(elemento)) // se van agregando los valores por medio de push que toma el call back al nuevo array
+  })
+  
+  return newArray*/
+  
+  //Metodo con la sintaxis de una Arrow function
+  array.map(elemento => newArray.push(cb(elemento)));
+  
+  return newArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+
+  return newArray = array.filter((array) => (array.charAt(0)==='a'));
+
 }
 
 // No modificar nada debajo de esta línea
